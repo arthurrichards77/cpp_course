88454970cb26dbcafd732d91cc5cd467810a8cdb
Arthur <arthur.richards@bristol.ac.uk>
2018-09-20 18:45:01 +0100

    Minor tidy

--- rectanglelibtest.cpp
+++ rectanglelibtest.cpp
@@ -3,8 +3,6 @@
 
 int main() {
   Rectangle r;
-  // can't access h and w directly from outside
-  // r.w = 3.0 will fail
   r.set_width_height(3.0,4.0);
   std::cout << r.get_area() << std::endl;
   r.scale(2.0);
--- squarelib.cpp
+++ squarelib.cpp
@@ -1,6 +1,5 @@
 #include "squarelib.hpp"
 
 void Square::set_side_len(double s_in) {
-  // can't access h,w directly as they're private
   set_width_height(s_in,s_in);
 }
