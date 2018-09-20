33611f1954823d1c5688d2c1069b05c368099a78
Arthur <arthur.richards@gmail.com>
2018-09-20 21:02:48 +0100

    Simplified vector example by avoiding iterators

--- poly.cpp
+++ poly.cpp
@@ -5,22 +5,21 @@
 int main() {
 
   std::vector<Rectangle> all_rects;
-  std::vector<Rectangle>::iterator it;
-
   Rectangle r;
+  Square s;
+  int ii;
+
   r.set_width_height(2.0,3.0);
   all_rects.push_back(r);
 
   r.set_width_height(5.0,3.0);
   all_rects.push_back(r);
 
-  Square s;
   s.set_side_len(3.0);
   all_rects.push_back(s);
 
-  for (it = all_rects.begin(); it != all_rects.end(); ++it) {
-    it->scale(2.0);
-    std::cout << it->get_area() << std::endl;
+  for (ii=0; ii<all_rects.size(); ii++) {
+    std::cout << all_rects[ii].get_area() << std::endl;
   }
 
   return(0);
--- rectlist.cpp
+++ rectlist.cpp
@@ -5,23 +5,21 @@
 int main() {
 
   std::vector<Rectangle> all_rects;
-  std::vector<Rectangle>::iterator it;
+  Rectangle r;
+  int ii;
 
-  Rectangle r1;
-  r1.set_width_height(2.0,3.0);
-  all_rects.push_back(r1);
+  r.set_width_height(2.0,3.0);
+  all_rects.push_back(r);
 
-  r1.set_width_height(5.0,3.0);
-  all_rects.push_back(r1);
+  r.set_width_height(5.0,3.0);
+  all_rects.push_back(r);
 
   std::cout << all_rects[0].get_area() << std::endl;
   std::cout << all_rects[1].get_area() << std::endl;
 
-  for (it = all_rects.begin(); it != all_rects.end(); ++it) {
-    //(*it).scale(2.0);
-    it->scale(2.0);
-    //std::cout << (*it).get_area() << std::endl;
-    std::cout << it->get_area() << std::endl;
+  for (ii=0; ii<all_rects.size(); ii++) {
+    all_rects[ii].scale(2.0);
+    std::cout << all_rects[ii].get_area() << std::endl;
   }
 
   return(0);
